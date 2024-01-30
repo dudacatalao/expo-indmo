3f7482a5cae1c7ae78e36a428be0e59f752809ab
dudacatalao <dmelocatalao@gamil.com>
2024-01-30 11:27:39 -0300

    first

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ViewBase } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style = {styles.acima}>
+        <Text style={styles.texto}>DUDA</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style = {styles.abaixo}>
+        <Text style={styles.texto}>LINDA</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#7B7B7B",
+    alignItems: "center",
+    justifyContent: "space-around",
+    
+  },
+
+  acima:{
+    backgroundColor: '#FF009E',
+    flex: 0.2,
+    width: '90%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  abaixo:{
+    backgroundColor: '#FFA6DD',
+    flex: 0.2,
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  texto:{
+    color: 'white'
+  },
+
 });
